3040226af3bd7647815afd0c21faf90e6658d093
Shemlenie <62347381+Shemlenie@users.noreply.github.com>
2020-06-14 00:24:48 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -2,6 +2,8 @@ print("Hello world")
 from flask import Flask
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+SECRET_KEY = 'secret'
+app.config['SECRET_KEY'] = SECRET_KEY
 
 #декоратор для вывода страницы по умолчанию
 @app.route("/")
